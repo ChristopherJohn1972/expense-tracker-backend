7536c3866a81f63434023f456703ffc63190ba05
ChristopherJohn1972 <mutukuchristopher562@gmail.com>
2025-08-08 13:32:51 +0300

    Update main.py to import app from FastAPIBackend.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, status, UploadFile, File
+from FastAPIBackend import app, Depends, HTTPException, status, UploadFile, File
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
