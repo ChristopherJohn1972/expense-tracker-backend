3afbe1673f57eb3685f79e560c18efdd3ad67aef
ChristopherJohn1972 <mutukuchristopher562@gmail.com>
2025-08-08 22:43:35 +0300

    Remove build.sh file

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Upgrade pip and install dependencies
-pip install --upgrade pip setuptools wheel
-
-# Install PyAudio using a pre-built wheel (no system dependencies needed)
-pip install PyAudio --global-option='--global-option=-I/usr/include/x86_64-linux-gnu/'
-
-# Install other requirements
-pip install -r requirements.txt
